48489a9a78b0eedfadb928d05694707e4b1dce8f
Shivam Daxesh Patel <spatel95@hydra15.eecs.utk.edu>
2019-03-28 01:03:36 -0400

    units and balancing

---
+++ ShivamChallenge8/Units.cpp
@@ -0,0 +1,84 @@
+// Description: Implementing the units and its attacks 
+
+
+using namespace std;
+#include "Units.h"
+#include <iostream>
+
+//*****UNIT -----> SNIPER*******
+Sniper::Sniper(){
+	this->health = 2500;
+	this->damage = 550;
+	this->range = 10;
+}
+
+int Sniper::getHealth(){return health;}
+
+int Sniper::getDamage(){return damage;}
+
+int Sniper::getRange(){return range;}
+
+void Sniper::print(){
+	cout<<"Sniper Status:"<<endl<<"health: "<<health<<endl<<"damage: "<<damage<<endl<<"range: "<<range<<endl;
+}
+
+//*****UNIT -----> ARTILLERY*******
+Artillery::Artillery(){
+	this->health = 6000;
+	this->damage = 250;
+	this->range = 2;
+}
+
+int Artillery::getHealth(){return health;}
+
+int Artillery::getDamage(){return damage;}
+
+int Artillery::getRange(){return range;}
+
+void Artillery::print(){
+	cout<<"Artillery Status:"<<endl<<"health: "<<health<<endl<<"damage: "<<damage<<endl<<"range: "<<range<<endl;
+}
+
+//*****UNIT -----> INFANTRY*******
+Infantry::Infantry(){
+	this->health = 3000;
+	this->damage = 1100;
+	this->range = 1;
+}
+
+int Infantry::getHealth(){return health;}
+
+int Infantry::getDamage(){return damage;}
+
+int Infantry::getRange(){return range;}
+
+void Infantry::print(){
+	cout<<"Infantry Status:"<<endl<<"health: "<<health<<endl<<"damage: "<<damage<<endl<<"range: "<<range<<endl;
+}
+
+//*****UNIT -----> CAVALRY*******
+Cavalry::Cavalry(int rowsize){
+	this->health = 4000;
+	this->damage = 375;
+	this->range = rowsize;
+}
+
+int Cavalry::getHealth(){return health;}
+
+int Cavalry::getDamage(){return damage;}
+
+int Cavalry::getRange(){return range;}
+
+void Cavalry::print(){
+	cout<<"Cavalry Status:"<<endl<<"health: "<<health<<endl<<"damage: "<<damage<<endl<<"range: "<<range<<endl;
+}
+int main(){
+	Cavalry c(35);
+	Unit *unit1 = &c;	
+	int health=	unit1->getHealth();	
+	int damage = unit1->getDamage();
+	int range = unit1->getRange();
+	unit1->print();
+	return 0;
+
+}//end of main
