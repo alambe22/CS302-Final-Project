38770626c43a786296b12e0b21da2474d88237a4
Alexander Lambert <alambe22@hydra15.eecs.utk.edu>
2019-03-29 16:29:02 -0400

    Finished commenting

--- alexChallenge8/Unit.h
+++ alexChallenge8/Unit.h
@@ -1,14 +1,22 @@
+//Header file for unit library
+
 #include <string>
 using namespace std;
+
+//All units are derived from a basic class, that way they all
+//share similar functions and can be easily stored together in the same container using
+//pointers.
 class Unit{
 public:
 	virtual int getMovesLeft() =0;
 	virtual int getMaxMoves()=0;
 	virtual int getUsedMoves()=0;
 	virtual void setUsedMoves(int usedMoves)=0;
-	virtual bool move(char direction, int numTiles) =0;
+	virtual bool move(int row, int col) =0;
 };
 
+// Main Base class. Cannot move, but can attack. Future versions
+// will add a health to this, which will decide who wins and loses.
 class Base : public Unit{
 public:
 	Base(int r, int c);
@@ -27,6 +35,9 @@ private:
 	int usedMoves;
 };
 
+
+// Basic attack unit. Can attack and move 1 tile. Future versions
+// will allow this to move, and attack other tiles.
 class Infantry : public Unit{
 public:
 	Infantry(int r, int c);
