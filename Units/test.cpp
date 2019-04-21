aeb58c1974bf4e3ee33804cdc439626715e6d348
Shivam Daxesh Patel <spatel95@hydra15.eecs.utk.edu>
2019-04-21 00:19:42 -0400

    vector<vector<char> >

--- Units/test.cpp
+++ Units/test.cpp
@@ -6,20 +6,21 @@ using namespace std;
 int main(){
 
 	Sniper i(5,5);
-	Base g(3,5);
 	Unit *unit1 = &i;
-	Unit *unit2 = &g;
+
 	pair<int,int> des;
-	des = make_pair(20,20);
+	des = make_pair(4,5);
 	int row =20;
 	int col =20;
 	int visibility = 1;
 	bool move;
-	vector<Unit*> units;
-	units.push_back(unit1);
-	units.push_back(unit2);
-
-	move=unit1->attack(visibility, unit2);
-	unit2->print();
+	vector<vector<char> >grid;
+	grid.resize(row);
+	for(int i=0; i<grid.size(); i++){
+		grid[i].resize(col,'_');
+	}
+	grid[5][5] = 'A';
+	move=unit1->move(grid, des, row, col);
+	unit1->print();
 	return 0;
 }
